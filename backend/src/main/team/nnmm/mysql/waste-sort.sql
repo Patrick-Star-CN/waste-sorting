7ae163ab7ddbdcb275eafa28827dcf6d9a1943cd
Patrick-Star-CN <1592810043@qq.com>
2022-04-09 19:17:44 +0800

    fix(mysql): change the waste dict data

--- backend/src/main/team/nnmm/mysql/waste-sort.sql
+++ backend/src/main/team/nnmm/mysql/waste-sort.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80026
  File Encoding         : 65001
 
- Date: 08/04/2022 20:45:12
+ Date: 09/04/2022 19:13:13
 */
 
 SET NAMES utf8mb4;
@@ -47,25 +47,25 @@ CREATE TABLE `wastedata`  (
 -- ----------------------------
 -- Records of wastedata
 -- ----------------------------
-INSERT INTO `wastedata` VALUES (1, '较完整的玻璃制品', 1, 2, 1);
-INSERT INTO `wastedata` VALUES (2, '小鸭子玩具', 1, 1, 1);
+INSERT INTO `wastedata` VALUES (1, '破花瓶', 1, 1, 2);
+INSERT INTO `wastedata` VALUES (2, '旧塑料玩具', 1, 1, 1);
 INSERT INTO `wastedata` VALUES (3, '旧衣服', 1, 2, 2);
 INSERT INTO `wastedata` VALUES (4, '毛绒玩具', 1, 2, 2);
 INSERT INTO `wastedata` VALUES (5, '旧书', 1, 2, 2);
 INSERT INTO `wastedata` VALUES (6, '易拉罐', 1, 1, 1);
-INSERT INTO `wastedata` VALUES (7, '皮鞋', 1, 1, 3);
-INSERT INTO `wastedata` VALUES (8, '大骨头', 2, 3, 1);
-INSERT INTO `wastedata` VALUES (9, '鱼骨', 2, 1, 2);
+INSERT INTO `wastedata` VALUES (7, '皮鞋', 1, 2, 1);
+INSERT INTO `wastedata` VALUES (8, '骨头', 2, 1, 2);
+INSERT INTO `wastedata` VALUES (9, '鱼骨', 2, 2, 1);
 INSERT INTO `wastedata` VALUES (10, '烟蒂', 3, 1, 1);
-INSERT INTO `wastedata` VALUES (11, '碎碗碟', 3, 1, 2);
-INSERT INTO `wastedata` VALUES (12, '电池', 4, 1, 1);
-INSERT INTO `wastedata` VALUES (13, '灯泡', 4, 1, 2);
+INSERT INTO `wastedata` VALUES (11, '碎碗碟', 3, 2, 1);
+INSERT INTO `wastedata` VALUES (12, '废电池', 4, 1, 1);
+INSERT INTO `wastedata` VALUES (13, '废灯泡', 4, 2, 1);
 INSERT INTO `wastedata` VALUES (14, '过期药物', 4, 1, 1);
-INSERT INTO `wastedata` VALUES (15, '牛奶纸盒', 1, 2, 2);
-INSERT INTO `wastedata` VALUES (16, '尿片', 3, 1, 2);
-INSERT INTO `wastedata` VALUES (17, '菜叶', 2, 2, 1);
-INSERT INTO `wastedata` VALUES (18, '脸盆', 1, 2, 1);
-INSERT INTO `wastedata` VALUES (19, '废灯管', 4, 1, 3);
-INSERT INTO `wastedata` VALUES (20, '污损塑料袋', 3, 2, 2);
+INSERT INTO `wastedata` VALUES (15, '牛奶纸盒', 1, 1, 1);
+INSERT INTO `wastedata` VALUES (16, '污损塑料袋', 3, 2, 2);
+INSERT INTO `wastedata` VALUES (17, '卫生纸', 3, 2, 2);
+INSERT INTO `wastedata` VALUES (18, '菜叶', 2, 1, 2);
+INSERT INTO `wastedata` VALUES (19, '破脸盆', 1, 2, 1);
+INSERT INTO `wastedata` VALUES (20, '落叶', 2, 1, 1);
 
 SET FOREIGN_KEY_CHECKS = 1;
